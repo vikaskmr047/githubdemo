25bb05f61a6dae9180f5a65d816573d57d1d3a5d
vikaskmr047 <46374732+vikaskmr047@users.noreply.github.com>
2023-10-11 12:07:48 +0530

    change plan

--- main.tf
+++ main.tf
@@ -9,4 +9,8 @@ terraform {
 }
 provider "azurerm" {
   features {}
+  client_id = "21d5c04f-85d0-4397-a417-22108e9fc6d8"
+  client_secret = "4UG8Q~x-k-F-7BncXalwIkd8bNas3N8LyvIpeaEz"
+  tenant_id = "37c599a4-184b-496f-a6b1-9a8d8bbe40ba"
+  subscription_id = "b76fefda-dd9b-4b27-9231-0cb72e202232"
 }
